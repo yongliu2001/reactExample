fa750bc8b13678bb512cae13f28c775bd641204f
Yong Liu <yong.liu2001@gmail.com>
2016-01-24 16:08:19 +1100

    add onChange and onRemove method to board component

--- ReactComponents/js/Note.js
+++ ReactComponents/js/Note.js
@@ -8,13 +8,12 @@ var Note = React.createClass({
   },
 
   save: function() {
-    var val = this.refs.newText.getDOMNode().value;
-    alert('TODO:// save value '+val);
+    this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
     this.setState({editing: false});
   },
 
   remove: function() {
-    alert('removing note');
+    this.props.onRemove(this.props.index);
   },
 
   renderDisplay: function() {
@@ -70,14 +69,30 @@ var Board = React.createClass({
       };
   },
 
+  update:function(newText, i) {
+    var arr = this.state.notes;
+    arr[i] = newText;
+    this.setState({notes: arr});
+  },
+
+  remove: function(i) {
+    var arr = this.state.notes;
+    arr.splice(i, 1);
+    this.setState({notes: arr});
+  },
+
+  eachNote: function(note, i) {
+    return (
+      <Note key={i} index={i}
+            onChange={this.update}
+            onRemove={this.remove}>{note}</Note>
+    );
+  },
+
   render: function() {
     return (
       <div className="board">
-        {this.state.notes.map(function(note,i){
-          return (
-            <Note key={i}>{note}</Note>
-          );
-        })}
+        {this.state.notes.map(this.eachNote)}
       </div>
     );
   }
